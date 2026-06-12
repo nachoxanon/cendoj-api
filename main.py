b81d7a088859031b09a0c4550c32913f29d105ed
nachoxanon <56862808+nachoxanon@users.noreply.github.com>
2026-06-12 13:38:45 +0200

    Refactor error handling and clean up imports
    Reemplazando código

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from bson.json_util import dumps
 import random
-
-#from bson.json_util import dumps
-from flask import Flask, jsonify
-from flask import Flask, redirect, url_for, request, render_template, make_response
-#from Tools.mongo_tools import all_sentences_byname
+from flask import Flask, jsonify, request, render_template
 import markdown.extensions.fenced_code
 from Tools.sql_tools import *
 
@@ -13,9 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     readme_file = open("README.md", "r")
-    md_template = markdown.markdown(readme_file.read(), extensions = ["fenced_code"])
+    md_template = markdown.markdown(readme_file.read(), extensions=["fenced_code"])
     return md_template
-
 
 @app.route("/all")
 def all_from_sql():
@@ -23,12 +17,11 @@ def all_from_sql():
         lines = get_all_from_sql()
         return jsonify(lines)
     except Exception as e:
-        return dumps({'error': str(e)})
+        return jsonify({'error': str(e)})
 
 @app.route("/random-number")
 def random_number():
-    return str(random.choice(range(0,1000)))
-
+    return str(random.choice(range(0, 1000)))
 
 @app.route("/count/<variable>")
 def count_with_variable(variable):
@@ -36,8 +29,7 @@ def count_with_variable(variable):
         lines = get_count_with_variable(variable)
         return jsonify(lines)
     except Exception as e:
-        return dumps({'error': str(e)})
-
+        return jsonify({'error': str(e)})
 
 @app.route("/all/<variable>/<name>")
 def all_with_variable(variable, name):
@@ -45,14 +37,11 @@ def all_with_variable(variable, name):
         lines = get_all_with_variable(variable, name)
         return jsonify(lines)
     except Exception as e:
-        return dumps({'error': str(e)})
-
-
+        return jsonify({'error': str(e)})
 
 @app.route("/post/", methods=['POST', 'GET'])
 def gfg():
     if request.method == "POST":
-        # getting input with name = fname in HTML form
         link = request.form.get("fname")
         downloading_sentence(link)
         data_sentencia = regex_court_sentence_file()
@@ -60,6 +49,7 @@ def gfg():
         return jsonify(data_sentencia)
     return render_template("login.html")
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
 if __name__ == '__main__':
     app.run(debug=True)
