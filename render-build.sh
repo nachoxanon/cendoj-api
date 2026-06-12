3dada178fc3e957aa70a58cbea2cdb8a3a2d6737
nachoxanon <56862808+nachoxanon@users.noreply.github.com>
2026-06-12 14:37:01 +0200

    Modify Chrome and ChromeDriver download in render-build.sh
    Updated Chrome and ChromeDriver download process to use specific versions and changed the download method from .deb to .zip format.

--- render-build.sh
+++ render-build.sh
@@ -2,17 +2,31 @@
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
+CHROME_VERSION=131.0.6778.264
+CHROMEDRIVER_VERSION=131.0.6778.264
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
+  echo "...Downloading Chrome v$CHROME_VERSION"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
+  wget -P ./ "https://storage.googleapis.com/chrome-for-testing-public/$CHROME_VERSION/linux64/chrome-linux64.zip"
+  unzip chrome-linux64.zip
+  rm chrome-linux64.zip
   cd $HOME/project/src
 else
   echo "...Using Chrome from cache"
 fi
 
-# La línea de 'pip install' ya no está aquí
+if [[ ! -d $STORAGE_DIR/chromedriver ]]; then
+  echo "...Downloading ChromeDriver v$CHROMEDRIVER_VERSION"
+  mkdir -p $STORAGE_DIR/chromedriver
+  cd $STORAGE_DIR/chromedriver
+  wget -P ./ "https://storage.googleapis.com/chrome-for-testing-public/$CHROMEDRIVER_VERSION/linux64/chromedriver-linux64.zip"
+  unzip chromedriver-linux64.zip
+  rm chromedriver-linux64.zip
+  cd $HOME/project/src
+else
+  echo "...Using ChromeDriver from cache"
+fi
+
+# No necesitas instalar dependencias aquí si ya lo haces en el Build Command de Render
